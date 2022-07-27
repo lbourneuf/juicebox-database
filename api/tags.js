7d5ce3c5efb7b12949c06e46fbcd50dd24ade461
Luke Bourneuf <lukebourneuf@gmail.com>
2022-07-27 11:43:01 -0400

    part 3 finished

--- api/tags.js
+++ api/tags.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const tagsRouter = express.Router();
 const { getAllTags } = require("../db");
-
+const { getPostsByTagName } = require("../db");
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
 
@@ -12,6 +12,20 @@ tagsRouter.get("/", async (req, res) => {
   const tags = await getAllTags();
 
   res.send({ tags });
+});
+
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  const { tagName } = req.params;
+
+  try {
+    const tags = await getPostsByTagName(tagName);
+    res.send({ posts: tags });
+  } catch ({ name, message }) {
+    next({
+      name: "FailedToGetTags",
+      message: "failed to get posts from the specified tags",
+    });
+  }
 });
 
 module.exports = tagsRouter;
